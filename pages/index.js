8bf2b996c26c3982dd7b105889358e65598386f5
Abiona Michael <amabiona21@gmail.com>
2021-03-22 14:13:15 +0100

    fetching data from an external api

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,46 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-const Home = () => {
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const result = await res.json();
+  
+  return {
+    props: { posts: result.slice(0, 10) }
+  }
+}
+
+const Home = ({ posts }) => {
   return (
-    <div className={styles.container}>
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.container}>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to <a href="/">Blogger.js!</a></h1>
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <main className={styles.main}>
+          <h1 className={styles.title}>Welcome to <a href="/">Blogger.js!</a></h1>
+          <p className={styles.description}>
+            Get started by editing{' '}
+            <code className={styles.code}>pages/index.js</code>
+          </p>
 
-        <div className={styles.grid}>
-          <a href="/" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+          <div className={styles.grid}>
+            {posts.map(post => (
+              <a key={post.id} href="/" className={styles.card}>
+                <h3>{post.title} &rarr;</h3>
+                <p>{post.body}</p>
+              </a>
+            ))}
+          </div>
+        </main>
 
-          <a href="/" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="/" className={styles.card}>
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a href="/" className={styles.card}>
-            <h3>Deploy &rarr;</h3>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a href="/" target="_blank" rel="noopener noreferrer"> Powered by{' '} <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /></a>
-      </footer>
-    </div>
+        <footer className={styles.footer}>
+          <a href="/" target="_blank" rel="noopener noreferrer"> Powered by{' '} <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} /></a>
+        </footer>
+      </div>
+    </>
   )
 }
 
